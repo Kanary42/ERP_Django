0d3ed7ace49f390a3b17b1ae1ece910509d655a8
Kanary42 <Kanary42@users.noreply.github.com>
2023-02-09 20:45:35 +0500

    Commit

--- ERP_Django/ERP_Django/urls.py
+++ ERP_Django/ERP_Django/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 
-if  settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
